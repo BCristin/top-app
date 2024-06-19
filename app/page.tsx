17de010d9a91996aa3497d87017284e1452648f9
Cristin <cristinnbalan@gmail.com>
2024-06-20 01:40:00 +0300

    componet Button + Htag

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from 'next';
 import styles from './page.module.css';
 
+import { Htag } from '@/components';
+import { Button } from '@/components/Button/Button';
 import { Noto_Sans } from 'next/font/google';
 const notoSans900 = Noto_Sans({ subsets: ['latin'], weight: '900' });
-const notoSans800 = Noto_Sans({ subsets: ['latin'], weight: '800' });
-const notoSans700 = Noto_Sans({ subsets: ['latin'], weight: '700' });
-const notoSans600 = Noto_Sans({ subsets: ['latin'], weight: '600' });
-const notoSans500 = Noto_Sans({ subsets: ['latin'], weight: '500' });
-const notoSans400 = Noto_Sans({ subsets: ['latin'], weight: '400' });
-const notoSans300 = Noto_Sans({ subsets: ['latin'], weight: '300' });
-const notoSans200 = Noto_Sans({ subsets: ['latin'], weight: '200' });
-const notoSans100 = Noto_Sans({ subsets: ['latin'], weight: '100' });
 
 export const metadata: Metadata = {
 	title: 'page',
@@ -18,16 +12,15 @@ export const metadata: Metadata = {
 };
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<div className={notoSans900.className}>salut</div>
-			<div className={notoSans800.className}>salut</div>
-			<div className={notoSans700.className}>salut</div>
-			<div className={notoSans600.className}>salut</div>
-			<div className={notoSans500.className}>salut</div>
-			<div className={notoSans400.className}>salut</div>
-			<div className={notoSans300.className}>salut</div>
-			<div className={notoSans200.className}>salut</div>
-			<div className={notoSans100.className}>salut</div>
+		<main className={styles}>
+			<Htag tag="h1">Курсы по Photoshop </Htag>
+			<Button apperance="primary">Узнать подробнее</Button>
+			<Button apperance="ghost" arrow="down">
+				Узнать подробнее
+			</Button>
+			<Button apperance="ghost" arrow="right">
+				Узнать подробнее
+			</Button>
 		</main>
 	);
 }
---
+++ components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
+import ArrowIcon from './arrow.svg';
+
+export const Button = ({
+	apperance,
+	arrow = 'none',
+	children,
+	className,
+	...props
+}: ButtonProps): JSX.Element => {
+	return (
+		<button
+			className={cn(styles.button, {
+				[styles.primary]: apperance == 'primary',
+				[styles.ghost]: apperance == 'ghost',
+			})}
+			{...props}>
+			{children}{' '}
+			{arrow !== 'none' && (
+				<span className={cn(styles.arrow, { [styles.down]: arrow == 'down' })}>
+					<ArrowIcon />
+				</span>
+			)}
+		</button>
+	);
+};
